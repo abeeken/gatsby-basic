b3b3794a31eddfc1f4159ff11a601d386576ef59
Andrew Beeken <abeeken@gmail.com>
2019-12-18 13:43:27 +0000

    Added Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         path: './src/img'
       }
     },
+    'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
